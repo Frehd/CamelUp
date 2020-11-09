2ac83581033c96fca6b7a0caff2ada88a497f566
Frehd <39314181+Frehd@users.noreply.github.com>
2020-11-09 16:38:19 +0100

    Implemented camel moving + dice throwing

--- src/Camels.hs
+++ src/Camels.hs
@@ -1,5 +1,10 @@
-module Camels where
+module Camels (Camel (..), moveCamel) where
+
+import Control.Lens.Combinators (element)
+import Control.Lens.Operators
+import Data.List
 import qualified Data.Vector as Vec
+import Pieces (PieceState (CamelStack))
 
 newtype Camel = Camel Int
   deriving (Eq, Ord)
@@ -14,7 +19,49 @@ instance Show Camel where
   show (Camel a) = show a ++ " (unknown camelNum)"
 
 splitCamelStack :: Vec.Vector Camel -> Camel -> (Vec.Vector Camel, Vec.Vector Camel) --(leave behind, camels to move)
-splitCamelStack camelStack camel' = (Vec.take camelIndex camelStack, Vec.drop camelIndex camelStack) where
-  camelIndex = case Vec.elemIndex camel' camelStack of
-    Just i -> i
-    Nothing -> -1
+splitCamelStack camelStack camel = (Vec.take camelIndex camelStack, Vec.drop camelIndex camelStack)
+  where
+    camelIndex = case Vec.elemIndex camel camelStack of
+      Just i -> i
+      Nothing -> -1
+
+containsCamel :: PieceState -> Camel -> Bool
+containsCamel (CamelStack camelVec) camel = Vec.elem camel camelVec
+containsCamel _ _ = False
+
+findCamel :: Camel -> [PieceState] -> Int
+findCamel camel pieceStates = case Data.List.findIndex (`containsCamel` camel) pieceStates of
+  Just a -> a
+  Nothing -> -1
+
+removeCamels :: Vec.Vector Camel -> [PieceState] -> [PieceState]
+removeCamels removalCamels pieceStates = case pieceStates !! camelPos of
+  CamelStack camelVec -> (element camelPos .~ CamelStack standingCamels) pieceStates
+    where
+      (standingCamels, _) = splitCamelStack camelVec $ Vec.head removalCamels
+  where
+    camelPos = findCamel (Vec.head removalCamels) pieceStates
+
+addCamels :: Vec.Vector Camel -> Int -> [PieceState] -> [PieceState]
+addCamels camels pos pieceStates = (element pos .~ CamelStack a) pieceStates
+  where
+    a = case pieceStates !! pos of
+      CamelStack camelVec -> camelVec Vec.++ camels
+
+moveCamel :: Camel -> Int -> [PieceState] -> [PieceState]
+moveCamel camel spaces pieceStates = case pieceStates !! camelPos of
+  CamelStack camelVec ->
+    addCamels movingCamels (camelPos + spaces) $
+      removeCamels standingCamels pieceStates
+    where
+      (standingCamels, movingCamels) = splitCamelStack camelVec camel
+  where
+    camelPos = findCamel camel pieceStates
+
+moveCamel' :: Camel -> Int -> [PieceState] -> [PieceState]
+moveCamel' camel spaces pieceStates = case pieceStates !! camelPos of --todo test if the same as moveCamel
+  CamelStack camelVec -> (element camelPos .~ CamelStack standingCamels) pieceStates
+    where
+      (standingCamels, movingCamels) = splitCamelStack camelVec camel --splitCamelStack camelVec camel
+  where
+    camelPos = findCamel camel pieceStates
--- src/Moves.hs
+++ src/Moves.hs
@@ -1,17 +1,26 @@
 module Moves (Move (..), execMove, possibleMoves) where
 
 import Bets
-import Data.Set (Set, empty, fromList, insert, map, singleton, union)
+import Camels
+import Data.Set (Set, empty, fromList, insert, map, singleton, toList, union)
+import Dice
 import GameState
 import Plates
 
 data Move = ThrowDice | RoundBet Bet | GameBet Bet | LayPlate PlateState
   deriving (Eq, Ord, Show)
 
+throwDice :: GameState -> [GameState]
+throwDice gameState =
+  [ gameState {pieceState = moveCamel camel spaces (GameState.pieceState gameState), GameState.diceState = markDiceThrown camel (GameState.diceState gameState)}
+    | camel <- Data.Set.toList $ possibleDice gameState,
+      spaces <- [1 .. 3]
+  ]
+
 execMove :: Move -> GameState -> Set GameState
 execMove (LayPlate plateState) gameState = Data.Set.singleton gameState {pieceState = addPlate (pieceState gameState) plateState (turn gameState)}
 execMove (RoundBet bet) gameState = Data.Set.singleton (gameState :: GameState) {GameState.betState = takeBet bet (GameState.betState gameState)}
-execMove ThrowDice gameState = Data.Set.fromList [gameState]
+execMove ThrowDice gameState = Data.Set.fromList $ throwDice gameState
 execMove _ _ = Data.Set.empty
 
 possibleMoves :: GameState -> Set Move
