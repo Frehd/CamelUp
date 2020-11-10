dfcb3e8437e9f109d906756e235736311410a829
Frehd <39314181+Frehd@users.noreply.github.com>
2020-11-10 15:21:11 +0100

    Added player addMoney helper function

--- src/Players.hs
+++ src/Players.hs
@@ -1,4 +1,4 @@
-module Players (Player (..)) where
+module Players (Player (..), addMoney) where
 
 import {-# SOURCE #-} Bets
 import Money
@@ -6,3 +6,6 @@ import {-# SOURCE #-} Plates
 
 data Player = Player {playerId :: Int, money :: Money, plate :: PlayerPlateState, bets :: [Bet]}
   deriving (Eq, Ord, Show)
+
+addMoney :: Int -> Player -> Player
+addMoney amount player = player {money = money player + amount}
