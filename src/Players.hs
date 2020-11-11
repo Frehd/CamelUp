dda31e57a8a5e331a891a59ced03540d2cf173a6
Frehd <39314181+Frehd@users.noreply.github.com>
2020-11-11 19:04:52 +0100

    Added player round resets

--- src/Players.hs
+++ src/Players.hs
@@ -1,4 +1,4 @@
-module Players (Player (..), PlayerId (..), addMoney, getPlayer, getPlayerIndex) where
+module Players (Player (..), PlayerId (..), addMoney, getPlayer, getPlayerIndex, resetPlayerPlates, resetPlayerBets) where
 
 import {-# SOURCE #-} Bets
 import Data.List (findIndex)
@@ -13,6 +13,18 @@ data Player = Player {playerId :: PlayerId, money :: Money, plate :: PlayerPlate
 
 addMoney :: Int -> Player -> Player
 addMoney amount player = player {money = money player + amount}
+
+resetPlayerPlate :: Player -> Player
+resetPlayerPlate player = player {plate = Unused}
+
+resetPlayerPlates :: [Player] -> [Player]
+resetPlayerPlates = map resetPlayerPlate
+
+resetPlayerBet :: Player -> Player
+resetPlayerBet player = player {bets = []}
+
+resetPlayerBets :: [Player] -> [Player]
+resetPlayerBets = map resetPlayerBet
 
 getPlayerIndex :: PlayerId -> [Player] -> Int
 getPlayerIndex playerId' players = case findIndex (\player -> playerId player == playerId') players of
